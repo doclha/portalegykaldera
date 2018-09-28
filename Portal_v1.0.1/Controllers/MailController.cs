9a4f232a9c37ab0886fb221a67eb2e1f5c9c6b56
Doğukan Culha <doclha@outlook.com>
2018-09-28 11:47:18 +0300

    Değişikliklerin kullanıcıya bildirilmesi eklendi

---
+++ Portal_v1.0.1/Controllers/MailController.cs
@@ -0,0 +1,57 @@
+﻿using Portal_v1._0._1.Identity;
+using Portal_v1._0._1.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+
+namespace Portal_v1._0._1.Controllers
+{
+    public class MailController
+    {
+        public void MailGonderAsync(string mesaj, string key)
+        {
+           
+            if (!String.IsNullOrEmpty(key))
+            {
+
+                var message = new MailMessage();
+                if (key == "izin")
+                {
+                    message.To.Add(new MailAddress(ConfigurationManager.AppSettings["PortalIzinMailAdres"]));
+                    message.To.Add(new MailAddress(ConfigurationManager.AppSettings["KalderaMailAdres"]));
+                }
+                else if (key == "masraf")
+                {
+                    message.To.Add(new MailAddress(ConfigurationManager.AppSettings["PortalMasrafMailAdres"]));
+                    message.To.Add(new MailAddress(ConfigurationManager.AppSettings["KalderaMailAdres"]));
+                }
+                else
+                {
+                    message.To.Add(new MailAddress(key));
+                }
+                message.From = new MailAddress("kaldera.portal@gmail.com");  // replace with valid value
+                message.Subject = "Portal Mesajı";
+                message.Body = mesaj;
+                message.IsBodyHtml = true;
+
+                using (var smtp = new SmtpClient())
+                {
+                    var credential = new NetworkCredential
+                    {
+                        UserName = "kaldera.portal@gmail.com",
+                        Password = "kalderasoft123"
+                    };
+                    smtp.Credentials = credential;
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    smtp.Send(message);
+                }
+            }
+        }
+    }
+}
---
+++ Portal_v1.0.1/Models/Masraf.cs
@@ -0,0 +1,54 @@
+﻿using Portal_v1._0._1.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Portal_v1._0._1.Models
+{
+    public class MasrafUrun
+    {
+        public int Id { get; set; }
+        public string Tedarikci { get; set; }
+        public string Aciklama { get; set; }
+        public string Tutar { get; set; }
+        public DateTime Tarih { get; set; }
+
+        public int MasrafSepetId { get; set; }
+        public MasrafSepet Sepet { get; set; }
+        
+    }
+    public class MasrafSepet
+    {
+        public int Id { get; set; }
+        public string ToplamTutar { get; set; }
+        public bool Onaylandi { get; set; }
+
+        public List<MasrafUrun> Urunler { get; set; }
+        public string PortalUserId { get; set; }
+        public PortalUser User { get; set; }
+    }
+    public class MasrafGelen
+    {
+        public string Aciklama { get; set; }
+        public string Tutar { get; set; }
+        public DateTime Tarih { get; set; }
+        public string Tedarikci { get; set; }
+
+
+    }
+
+    public class SepetGelen
+    {
+        public int SepetId { get; set; }
+        public string ToplamTutar { get; set; }
+        public bool Onaylandi { get; set; }
+        public string Tedarikci { get; set; }
+
+        public string Name { get; set; }
+        public string LastName { get; set; }
+
+        public string Aciklama { get; set; }
+    }
+}
